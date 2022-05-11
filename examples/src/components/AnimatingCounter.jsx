c28bee7985811b5e03312135d122bcf31d5f5528
tomByrer <tombyrer@gmail.com>
2022-05-11 00:16:15 -0500

    fix ref, indentation

--- examples/src/components/AnimatingCounter.jsx
+++ examples/src/components/AnimatingCounter.jsx
@@ -6,49 +6,50 @@ import './AnimatingCounter.css';
 const ICON_STYLES = { color: 'white', width: '1.4rem', height: 'auto' };
 
 function AnimatedCounter() {
-    const { play } = createAnimation(
-			'.CounterOutput',
-			{ y: [5, 20, -10, 0], opacity: [1, 0, 0, 1] },
-			{ duration: 0.5 },
-    );
-    const { play: playDecrease } = createAnimation(
-			'.CounterOutput',
-			{ y: [0, -10, 20, 0], opacity: [1, 0, 0, 1] },
-			{ duration: 0.5 },
-    );
+	let refCounterOutput
+	const { play } = createAnimation(
+		()=>refCounterOutput,
+		{ y: [5, 20, -10, 0], opacity: [1, 0, 0, 1] },
+		{ duration: 0.5 },
+	);
+	const { play: playDecrease } = createAnimation(
+		()=>refCounterOutput,
+		{ y: [0, -10, 20, 0], opacity: [1, 0, 0, 1] },
+		{ duration: 0.5 },
+	);
 
-    const [counter, setCounter] = createSignal(0);
+	const [counter, setCounter] = createSignal(0);
 
-    const increase = () => {
-			play();
-			setTimeout(() => {
-				setCounter(counter() + 1);
-			}, 100);
-    };
-    const descrease = () => {
-			playDecrease();
-			setTimeout(() => {
-				setCounter(counter() - 1);
-			}, 100);
-    };
+	const increase = () => {
+		play();
+		setTimeout(() => {
+			setCounter(counter() + 1);
+		}, 100);
+	};
+	const descrease = () => {
+		playDecrease();
+		setTimeout(() => {
+			setCounter(counter() - 1);
+		}, 100);
+	};
 
-    return (
-			<div class="CounterContainer">
-				<div class="container">
-					<button onClick={increase}>
-						^
-							{/* <ChevronUpIcon style={ICON_STYLES} /> */}
-					</button>
+	return (
+		<div class="CounterContainer">
+			<div class="container">
+				<button onClick={increase}>
+					^
+						{/* <ChevronUpIcon style={ICON_STYLES} /> */}
+				</button>
 
-					<p class="CounterOutput">{counter()}</p>
+				<p ref={refCounterOutput}>{counter()}</p>
 
-					<button onClick={descrease}>
-						v
-							{/* <ChevronDownIcon style={ICON_STYLES} /> */}
-					</button>
-				</div>
+				<button onClick={descrease}>
+					v
+						{/* <ChevronDownIcon style={ICON_STYLES} /> */}
+				</button>
 			</div>
-    );
+		</div>
+	);
 }
 
 export default AnimatedCounter;
