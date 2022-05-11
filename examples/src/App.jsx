df12bedbc1de2c57f54d3c48d41a91970dbe0dcf
tomByrer <tombyrer@gmail.com>
2022-05-10 23:58:55 -0500

    fix Counter, working but animation isn't

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -1,9 +1,11 @@
+import AnimatingCounter from "./components/AnimatingCounter";
 import AnimatingList from "./components/AnimatingList";
 
 function App() {
 
 	return (
 		<div class="App">
+			<AnimatingCounter />
 			<AnimatingList />
 		</div>
 	);
--- examples/src/components/AnimatingCounter.jsx
+++ examples/src/components/AnimatingCounter.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/solid';
-import { useMotionAnimate } from '../../../../src/index';
-import './AnimatedCounter.css';
+import { createSignal } from "solid-js";
+// import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/solid';
+import { createAnimation } from 'motion-signals'
+import './AnimatingCounter.css';
 
 const ICON_STYLES = { color: 'white', width: '1.4rem', height: 'auto' };
 
 function AnimatedCounter() {
-    const currentCounter = React.useRef(null);
-    const { play } = useMotionAnimate(
-        currentCounter,
+    let refCurrentCounter
+    const { play } = createAnimation(
+        refCurrentCounter,
         { y: [5, 20, -10, 0], opacity: [1, 0, 0, 1] },
         { duration: 0.5 },
     );
-    const { play: playDecrease } = useMotionAnimate(
-        currentCounter,
+    const { play: playDecrease } = createAnimation(
+        refCurrentCounter,
         { y: [0, -10, 20, 0], opacity: [1, 0, 0, 1] },
         { duration: 0.5 },
     );
 
-    const [counter, setCounter] = React.useState(0);
+    const [counter, setCounter] = createSignal(0);
 
     const increase = () => {
         play();
         setTimeout(() => {
-            setCounter(counter + 1);
+            setCounter(counter() + 1);
         }, 100);
     };
     const descrease = () => {
         playDecrease();
         setTimeout(() => {
-            setCounter(counter - 1);
+            setCounter(counter() - 1);
         }, 100);
     };
 
@@ -37,13 +37,15 @@ function AnimatedCounter() {
         <div className="CounterContainer">
             <div className="container">
                 <button onClick={increase}>
-                    <ChevronUpIcon style={ICON_STYLES} />
+									^
+                    {/* <ChevronUpIcon style={ICON_STYLES} /> */}
                 </button>
 
-                <p ref={currentCounter}>{counter}</p>
+                <p ref={refCurrentCounter}>{counter()}</p>
 
                 <button onClick={descrease}>
-                    <ChevronDownIcon style={ICON_STYLES} />
+									v
+                    {/* <ChevronDownIcon style={ICON_STYLES} /> */}
                 </button>
             </div>
         </div>
