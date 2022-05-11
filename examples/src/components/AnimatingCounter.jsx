d28b98362a0ad1654f6b87a1f5ebde96ea3b20d8
tomByrer <tombyrer@gmail.com>
2022-05-11 00:12:05 -0500

    fix animation using class, formatting

--- examples/src/components/AnimatingCounter.jsx
+++ examples/src/components/AnimatingCounter.jsx
@@ -6,49 +6,48 @@ import './AnimatingCounter.css';
 const ICON_STYLES = { color: 'white', width: '1.4rem', height: 'auto' };
 
 function AnimatedCounter() {
-    let refCurrentCounter
     const { play } = createAnimation(
-        refCurrentCounter,
-        { y: [5, 20, -10, 0], opacity: [1, 0, 0, 1] },
-        { duration: 0.5 },
+			'.CounterOutput',
+			{ y: [5, 20, -10, 0], opacity: [1, 0, 0, 1] },
+			{ duration: 0.5 },
     );
     const { play: playDecrease } = createAnimation(
-        refCurrentCounter,
-        { y: [0, -10, 20, 0], opacity: [1, 0, 0, 1] },
-        { duration: 0.5 },
+			'.CounterOutput',
+			{ y: [0, -10, 20, 0], opacity: [1, 0, 0, 1] },
+			{ duration: 0.5 },
     );
 
     const [counter, setCounter] = createSignal(0);
 
     const increase = () => {
-        play();
-        setTimeout(() => {
-            setCounter(counter() + 1);
-        }, 100);
+			play();
+			setTimeout(() => {
+				setCounter(counter() + 1);
+			}, 100);
     };
     const descrease = () => {
-        playDecrease();
-        setTimeout(() => {
-            setCounter(counter() - 1);
-        }, 100);
+			playDecrease();
+			setTimeout(() => {
+				setCounter(counter() - 1);
+			}, 100);
     };
 
     return (
-        <div className="CounterContainer">
-            <div className="container">
-                <button onClick={increase}>
-									^
-                    {/* <ChevronUpIcon style={ICON_STYLES} /> */}
-                </button>
+			<div class="CounterContainer">
+				<div class="container">
+					<button onClick={increase}>
+						^
+							{/* <ChevronUpIcon style={ICON_STYLES} /> */}
+					</button>
 
-                <p ref={refCurrentCounter}>{counter()}</p>
+					<p class="CounterOutput">{counter()}</p>
 
-                <button onClick={descrease}>
-									v
-                    {/* <ChevronDownIcon style={ICON_STYLES} /> */}
-                </button>
-            </div>
-        </div>
+					<button onClick={descrease}>
+						v
+							{/* <ChevronDownIcon style={ICON_STYLES} /> */}
+					</button>
+				</div>
+			</div>
     );
 }
 
