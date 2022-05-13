621f9d466b5b9050edfaf664fcf93faffda26700
tomByrer <tombyrer@gmail.com>
2022-05-13 00:19:10 -0500

    + AnimatingElements, but conflits with Counter

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -1,4 +1,5 @@
 import AnimatingCounter from "./components/AnimatingCounter";
+// import AnimatingElements from "./components/AnimatingElements";
 import AnimatingList from "./components/AnimatingList";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 	return (
 		<div class="App">
 			<AnimatingCounter />
+			{/* <AnimatingElements /> */}
 			<AnimatingList />
 		</div>
 	);
